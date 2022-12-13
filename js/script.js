6f66527ea538e8895632a30ba8ab18bee206e9b7
Lichfolky <lichfolky@gmail.com>
2022-12-13 10:22:53 +0100

    add date check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 let cells = document.querySelectorAll(".cell");
-for (cell of cells) {
-    cell.addEventListener("click", (e) => {
-        e.target.classList.toggle("show");
-    });
+let now = new Date();
+if (now.getMonth() == 11) {
+
+    for (cell of cells) {
+        let cellDate = new Date(now.getFullYear() + "-12-" + cell.innerText);
+        //console.log(cell.innerText + ": " + cellDate.toLocaleDateString());
+
+
+        if (now.getDate() == cellDate.getDate()) {
+            console.log(now.getDate(), cellDate.getDate());
+
+            //cell.classList.add("show");;
+            cell.addEventListener("click", (e) => {
+                e.target.classList.toggle("show");
+            });
+        }
+        if (now.getDate() > cellDate.getDate()) {
+            cell.classList.add("show");;
+        }
+    }
+
 }
